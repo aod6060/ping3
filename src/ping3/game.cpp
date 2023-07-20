3f6c4e4e1c4aef80190a815d01a47fe516f099a7
derf6060 <derf6060@derfnet.attlocal.net>
2023-07-20 00:04:25 -0500

    Finished up render system. For now.

--- src/ping3/game.cpp
+++ src/ping3/game.cpp
@@ -3,18 +3,44 @@
 
 
 namespace game {
+	
+
+
+    static float x = 32.0f;
+    static float y = 32.0f;
+
+    static float speed = 64.0f;
 
     void init() {
-        
     }
 
     void update(float delta) {
+	if(input::isKeyPress(input::Keyboard::KEYS_LEFT)) {
+		x -= speed * delta;
+	} else if(input::isKeyPress(input::Keyboard::KEYS_RIGHT)) {
+		x += speed * delta;
+	}
+
+	if(input::isKeyPress(input::Keyboard::KEYS_UP)) {
+		y -= speed * delta;
+	} else if(input::isKeyPress(input::Keyboard::KEYS_DOWN)) {
+		y += speed * delta;
+	}
     }
 
     void render() {
         render::clear(glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
         render::beginFrame();
+	
+	render::setProjection(glm::ortho(0.0f, (float)app::getWidth(), (float)app::getHeight(), 0.0f));
+	render::setView(glm::mat4(1.0f));
+	render::setModel(
+		glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0.0f)) *
+		glm::scale(glm::mat4(1.0f), glm::vec3(32.0f, 48.0f, 0.0f))		
+	);
 
+	render::render();
+	
         render::endFrame();
     }
 
--- src/ping3/render.cpp
+++ src/ping3/render.cpp
@@ -3,15 +3,103 @@
 
 namespace render {
 
+    // Shaders
+    static uint32_t main_vs = 0;
+    static uint32_t main_fs = 0;
+
+    // Program
+    static uint32_t main_p = 0;
+    static uint32_t u_proj = 0;
+    static uint32_t u_view = 0;
+    static uint32_t u_model = 0;
+
+    // Vertex Array
+    static uint32_t main_va = 0;
+    static uint32_t VERTICES = 0;
+
+
+    static std::vector<glm::vec3> vertices;
+    static uint32_t vertices_vbo = 0;
+
+    static std::vector<uint32_t> inds;
+    static uint32_t inds_vbo = 0;
+
+    uint32_t createShader(GLenum type, std::string path);
+    uint32_t createProgram(const std::vector<uint32_t>& shaders);
 
     void init() {
 
+        // Shader
+        main_vs = createShader(GL_VERTEX_SHADER, "./data/shaders/main.vert.glsl");
+        main_fs = createShader(GL_FRAGMENT_SHADER, "./data/shaders/main.frag.glsl");
+
+        // Program
+        main_p = createProgram({
+            main_vs,
+            main_fs
+        });
+
+        // Vertex Array
+        glGenVertexArrays(1, &main_va);
+
+        glUseProgram(main_p);
+
+        // Uniform
+        u_proj = glGetUniformLocation(main_p, "u_proj");
+        u_view = glGetUniformLocation(main_p, "u_view");
+        u_model = glGetUniformLocation(main_p, "u_model");
+
+        // Vertex Array
+        glBindVertexArray(main_va);
+        glEnableVertexAttribArray(VERTICES);
+        glBindVertexArray(0);
+
+        glUseProgram(0);
+
+
+        // Vertices
+        vertices.push_back(glm::vec3(0.0f, 0.0f, 0.0f));
+        vertices.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+        vertices.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+        vertices.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
+
+        glGenBuffers(1, &vertices_vbo);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vertices_vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_DYNAMIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        // Inds
+        inds = {
+            0, 1, 2,
+            2, 1, 3
+        };
+	
+	glGenBuffers(1, &inds_vbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, inds_vbo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, inds.size() * sizeof(uint32_t), inds.data(), GL_DYNAMIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
 
     void release() {
+        glDeleteBuffers(1, &inds_vbo);
+        glDeleteBuffers(1, &vertices_vbo);
 
+        glDeleteVertexArrays(1, &main_va);
+
+        std::vector<uint32_t> shaders = {
+            main_vs,
+            main_fs
+        };
+
+        std::for_each(shaders.begin(), shaders.end(), [](uint32_t id) {
+            glDetachShader(main_p, id);
+        });
+
+        glDeleteProgram(main_p);
+        glDeleteShader(main_fs);
+        glDeleteShader(main_vs);
     }
-
 
     void clear(glm::vec4 clearColor) {
         glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
@@ -19,28 +107,100 @@ namespace render {
     }
 
     void beginFrame() {
-
+        glUseProgram(main_p);
     }
 
     void endFrame() {
-
+        glUseProgram(0);
     }
     
     void setProjection(const glm::mat4& m) {
-
+        glUniformMatrix4fv(u_proj, 1, GL_FALSE, &m[0][0]);
     }
 
     void setView(const glm::mat4& m) {
-
+        glUniformMatrix4fv(u_view, 1, GL_FALSE, &m[0][0]);
     }
 
     void setModel(const glm::mat4& m) {
-
+        glUniformMatrix4fv(u_model, 1, GL_FALSE, &m[0][0]);
     }
 
     void render() {
+        glBindVertexArray(main_va);
 
+        glBindBuffer(GL_ARRAY_BUFFER, vertices_vbo);
+        glVertexAttribPointer(VERTICES, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, inds_vbo);
+        glDrawElements(GL_TRIANGLES, inds.size(), GL_UNSIGNED_INT, nullptr);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        glBindVertexArray(0);
     }
 
-}
 
+    uint32_t createShader(GLenum type, std::string path) {
+        uint32_t temp = glCreateShader(type);
+
+        std::stringstream ss;
+
+        util::loadFile(path, [&](std::string line) {
+            ss << line << "\n";
+        });
+
+        std::string src = ss.str();
+        const char* c_src = src.c_str();
+
+        glShaderSource(temp, 1, &c_src, nullptr);
+
+        glCompileShader(temp);
+
+        int len = 0;
+
+        glGetShaderiv(temp, GL_INFO_LOG_LENGTH, &len);
+
+        if(len > 0) {
+            std::string log;
+            log.resize(len);
+            glGetShaderInfoLog(temp, log.size(), nullptr, log.data());
+            logger::output(logger::LoggerType::LT_ERROR, [&](std::stringstream& ss) {
+                ss << "\n";
+                ss << log;
+                ss << "\n";
+            });
+            log.clear();
+        }
+
+        return temp;
+    }
+
+    uint32_t createProgram(const std::vector<uint32_t>& shaders) {
+        uint32_t temp = glCreateProgram();
+
+        std::for_each(shaders.begin(), shaders.end(), [&](uint32_t id) {
+            glAttachShader(temp, id);
+        });
+
+        glLinkProgram(temp);
+
+        int len = 0;
+
+        glGetProgramiv(temp, GL_INFO_LOG_LENGTH, &len);
+
+        if(len > 0) {
+            std::string log;
+            log.resize(len);
+            glGetProgramInfoLog(temp, log.size(), nullptr, log.data());
+            logger::output(logger::LoggerType::LT_ERROR, [&](std::stringstream& ss) {
+                ss << "\n";
+                ss << log;
+                ss << "\n";
+            });
+            log.clear();
+        }
+
+        return temp;
+    }
+}
